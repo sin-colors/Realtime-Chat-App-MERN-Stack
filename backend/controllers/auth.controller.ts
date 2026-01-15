a3d86e88fc897b7a59f6b663070175b01f7819e2
sin-colors <shingo6651inagaki2302@i3.gmobb.jp>
2026-01-16 00:44:35 +0900

    authControllerの作成

---
+++ backend/controllers/auth.controller.ts
@@ -0,0 +1,9 @@
+export function signup(req, res) {
+  console.log("signup User");
+}
+export function login(req, res) {
+  console.log("login User");
+}
+export function logout(req, res) {
+  console.log("logout User");
+}
--- backend/routes/auth.routes.ts
+++ backend/routes/auth.routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller";
+
 const router = express.Router();
-router.get("/login", (req, res) => {
-  res.send("Login Route");
-});
+router.get("/signup", signup);
+router.get("/login", login);
+router.get("/logout", logout);
 export default router;
