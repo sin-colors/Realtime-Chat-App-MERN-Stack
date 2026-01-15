e48ceb52961c64c360400946eb3956541c0cdc98
sin-colors <shingo6651inagaki2302@i3.gmobb.jp>
2026-01-16 00:35:13 +0900

    authRoutesの作成

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.use("/api/auth", authRoutes);
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
