b37322b97ec9e375c5e92b302db758c6947eb05e
sin-colors <shingo6651inagaki2302@i3.gmobb.jp>
2026-03-01 08:07:59 +0900

    メッセージを送信したときに最新のメッセージが見えるようにスクロールの設定を追加

--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -1,10 +1,19 @@
 import useGetMessages from "@/hooks/useGetMessages";
 import Message from "./Message";
 import MessageSkelton from "../skeltons/MessageSkelton";
+import { useEffect, useRef } from "react";
 
 function Messages() {
   const { messages, loading } = useGetMessages();
   // console.log("messages: ", messages);
+  const messageRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    setTimeout(() => {
+      // この設定だと一番上に表示されるような気がする。。。
+      // うまく機能しない場合は、scrollIntoViewのオプションにblock:"end"を追加する
+      messageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
   return (
     <div className="flex-1 overflow-auto px-4">
       {loading && [...Array(3)].map((_, idx) => <MessageSkelton key={idx} />)}
@@ -14,7 +23,9 @@ function Messages() {
       {!loading &&
         messages.length > 0 &&
         messages.map((message) => (
-          <Message key={message._id} message={message} />
+          <div key={message._id} ref={messageRef}>
+            <Message message={message} />
+          </div>
         ))}
     </div>
   );
